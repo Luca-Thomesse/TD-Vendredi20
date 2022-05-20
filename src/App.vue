bfd2d981dc32e1b797406e34a43ce9ce87896957
Luca-Thomesse <95016659+Luca-Thomesse@users.noreply.github.com>
2022-05-20 11:02:14 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <header>
+    <icon-logo />
+    <FilmIcon class="h-5 w-5 fill-blue-300 stroke-blue-600" />
+    <nav>
+      <RouterLink to="/" class="text-blue-600 underline">Accueil</RouterLink>
+    </nav>
+    <menu-logo>
+      
+    </menu-logo>
+  </header>
+
+  <RouterView />
+</template>
+
+<script>
+import IconLogo from "./components/icons/IconLogo.vue";
+import { FilmIcon } from "@heroicons/vue/outline";
+import { MenuIcon} from "@heroicons/vue/outline";
+export default {
+  components: { IconLogo, FilmIcon },
+  components: { IconLogo, MenuIcon },
+};
+</script>
---
+++ src/views/AccueilView.vue
@@ -0,0 +1,16 @@
+<template>
+  
+  <head>Estary</head>
+  
+  <header>
+     <h1 class="text-2xl ">Estary</h1>
+  </header>
+  
+  <main>
+    
+  </main>
+</template>
+
+<script>
+export default {};
+</script>
